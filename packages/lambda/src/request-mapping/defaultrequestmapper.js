cb74fcb75f70c34dd644c294c3b931e27cd25308
Andrew Kilgore <akilgore@moderndemocracy.com>
2017-12-19 10:02:07 +0000

    Add organisation to default request mapper

--- packages/lambda/src/request-mapping/defaultrequestmapper.js
+++ packages/lambda/src/request-mapping/defaultrequestmapper.js
@@ -7,6 +7,7 @@ class DefaultRequestMapper {
         lambdaBody.security.scopes = args[0].requestContext.authorizer.scope || '';
         lambdaBody.security.apiKey = args[0].requestContext.identity.apiKey || '';
         lambdaBody.security.principalId = args[0].requestContext.authorizer.principalId || '';
+        lambdaBody.security.organisation = args[0].requestContext.authorizer.organisation || '';
         lambdaBody.metadata.requestId = args[0].requestContext.requestId || '';
         lambdaBody.metadata.sourceIp = args[0].requestContext.identity.sourceIp || '';
         args[0] = lambdaBody;
