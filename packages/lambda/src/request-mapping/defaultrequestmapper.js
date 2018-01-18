464e4c35bf8e9fba6e8b8ea01316203de171fe34
Andrew Kilgore <akilgore@moderndemocracy.com>
2018-01-18 11:28:32 +0000

    Add acessToken to security map

--- packages/lambda/src/request-mapping/defaultrequestmapper.js
+++ packages/lambda/src/request-mapping/defaultrequestmapper.js
@@ -8,11 +8,27 @@ class DefaultRequestMapper {
         lambdaBody.security.apiKey = args[0].requestContext.identity.apiKey || '';
         lambdaBody.security.principalId = args[0].requestContext.authorizer.principalId || '';
         lambdaBody.security.organisation = args[0].requestContext.authorizer.organisation || '';
+        lambdaBody.security.accessToken = parseAuthorizationToken(args[0].headers.Authorization);
         lambdaBody.metadata.requestId = args[0].requestContext.requestId || '';
         lambdaBody.metadata.sourceIp = args[0].requestContext.identity.sourceIp || '';
         args[0] = lambdaBody;
         return args;
     }
+}
+
+function parseAuthorizationToken(token) {
+
+    if(!token) {
+        return ""
+    }
+
+    const matches = token.match(/^Bearer (.*)$/);
+
+    if(!matches || matches.length < 2) {
+        return ""
+    }
+
+    return matches[1];
 }
 
 export default DefaultRequestMapper;
